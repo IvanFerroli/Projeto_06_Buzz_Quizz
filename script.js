2fb3e82201603f90a3e2bd978f2e25fecf8b8256
Mateus <mateusoliveira469@gmail.com>
2022-02-18 18:57:14 -0400

    first-quiz-info

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 //<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 let indexAnswer = 0;
 let indexScroll = 0;
+let quizCreatedTitle = null;
+let quizCreatedURL = null;
+let quizCreatedNumberOfQuestions = null;
+let quizCreatedNumberOfLevels = null;
+let quizCorrectTitle = null;
+let quizCorrectURL = null;
+let quizCorrectNumOfQuestions = null;
+let quizCorrectNumOfLevels = null;
 
 function loadQuiz(){
     const request = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
@@ -96,13 +104,77 @@ function renderSingleAnswer(question, object){
 
 // Áre de Guerra. Cuidado! O que tem abaixo está tudo errado. Você foi avisado.
 
+//Botão: Inicia a criação do primeiro Quiz
 
 function insertSecondPage() {
-    let currentPage = document.querySelector('.second-page');
-    let lastPage = document.querySelector('.first-page');
+    let currentPage = document.querySelector('.main-2');
+    let lastPage = document.querySelector('.main-1');
     if (lastPage.style.display !== 'none') {    
         currentPage.style.display = 'block';
         lastPage.style.display = 'none';
+    }
+} 
+
+//Inicia criação de um Quiz
+
+
+function getQuizTitle (quizCreatedTitle) {
+    quizCreatedTitle = document.querySelector('.quiz-title').value;
+    console.log(quizCreatedTitle);
+    if ((quizCreatedTitle === null) || (quizCreatedTitle.length < 20) || (quizCreatedTitle.length > 65)) {
+        alert("Por favor, insira um título válido, que tenha entre 20 e 65 caracteres.");
+        quizCorrectTitle = false;
+    } else {
+        quizCorrectTitle = true;
+    }
+}
+
+function getQuizURL (quizCreatedURL) {
+    quizCreatedURL = document.querySelector('.quiz-image-url').value;
+    console.log(quizCreatedURL);
+    if ((quizCreatedURL !== null) &&  (quizCreatedURL.startsWith("https://") || quizCreatedURL.startsWith("http://"))) {
+        quizCorrectURL = true;
+    } else {
+        quizCorrectURL = false;
+        alert("Por favor, insira uma URL de imagem válida.");
+    }
+}
+
+function getNumberOfQuestions (quizCreatedNumberOfQuestions) {
+    quizCreatedNumberOfQuestions = document.querySelector('.quiz-number-of-questions').value;
+    console.log(quizCreatedNumberOfQuestions);
+    if ((quizCreatedNumberOfQuestions === null) || (quizCreatedNumberOfQuestions < 3)) {
+        alert("São necessárias ao menos 3 perguntas para a criação do quiz.");
+        quizCorrectNumOfQuestions = false;
+    } else {
+        quizCorrectNumOfQuestions = true;
+    }
+}
+
+function getNumberOfLevels (quizCreatedNumberOfLevels) {
+    quizCreatedNumberOfLevels = document.querySelector('.quiz-number-of-levels').value;
+    console.log(quizCreatedNumberOfLevels);
+    if ((quizCreatedNumberOfLevels === null) || (quizCreatedNumberOfLevels < 2)) {
+        alert("São necessários ao menos 2 níveis para a criação do quiz.");
+        quizCorrectNumOfLevels = false;
+    } else {
+        quizCorrectNumOfLevels = true;
+    }
+}
+
+function getQuizInfo () {
+    getQuizTitle();
+    getQuizURL();
+    getNumberOfQuestions();
+    getNumberOfLevels();
+
+    if ((quizCorrectTitle === true) && (quizCorrectURL === true) && (quizCorrectNumOfQuestions === true) && (quizCorrectNumOfLevels === true)) {
+        let currentPage = document.querySelector('.main-3');
+        let lastPage = document.querySelector('.main-2');
+        if (lastPage.style.display !== 'none') {    
+            currentPage.style.display = 'block';
+            lastPage.style.display = 'none';
+        }
     }
 }
 
